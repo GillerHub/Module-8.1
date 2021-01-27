c93903f7b86060aeaea06cb05cae0efb193d5fbc
Aleksandr Giller <alexander.p.17@yandex.ru>
2021-01-28 00:32:30 +0500

    Удаление файлов

--- Module 8.1/Program.cs
+++ Module 8.1/Program.cs
@@ -14,43 +14,41 @@ namespace DirectoryManager
              
             Console.WriteLine("Введите путь: ");
             var way = Console.ReadLine();
-            //Console.WriteLine("Передан некорректный путь");
+
             string dirName = way; // Прописываем путь к корневой директории
             
             if (Directory.Exists(dirName)) // Проверим, что директория существует
             {
                 Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
-
+                string[] dirs2 = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                string[] dirs1 = Directory.GetFiles(dirName, "*", SearchOption.AllDirectories);  // Получим все файлы корневого каталога
+                string[] dirs = new string[dirs1.Length + dirs2.Length];
+                dirs1.CopyTo(dirs, 0);
+                dirs2.CopyTo(dirs, dirs1.Length);
+                DateTime DT = DateTime.Now.Subtract(TimeSpan.FromMinutes(30));
+                Console.WriteLine(DT.ToString());
+                Console.WriteLine(DateTime.Now.ToString());
                 foreach (string d in dirs) // Выведем их все
+                { 
                     Console.WriteLine(d);
-
+                    Console.WriteLine(Directory.GetLastAccessTime(d).ToString());
+                    if (Directory.GetLastAccessTime(d) < DT)
+                    {
+                        if (d.Contains("F:/temp/d")) 
+                        {
+                            Directory.Delete(d, true);
+                            Console.WriteLine("Файл удалён " +d);
+                        } 
+                        else Console.WriteLine("Вы пытаетесь удалить нужный файл " +d);
+                    }
+                }
                 Console.WriteLine();
             }
             else
             {
                 Console.WriteLine("Папка по заданному адресу не существует");
             }
-
-            try
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(way);
-                Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-                if (dirInfo.Exists)
-                {
-
-                }
-            }
-            /*try
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo(way);
-            dirInfo.Delete(true); // Удаление со всем содержимым
-            Console.WriteLine("Каталог удален");
-        }*/
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            
         }
     }
 }
